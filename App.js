21c3252f08c59d3d60614aa0450b23cfb7ea26b7
Abhay Thanak <abhaythanak@gmail.com>
2023-12-19 12:12:13 +0530

    this how we create react app

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
   
-  const container = React.createElement("div",{id: "parent",},[
-    React.createElement("div",{id: "child",},[
-        React.createElement("h1",{},"heading1"),
-        React.createElement("h1",{},"heading2"),
-    ]),
-    React.createElement("div",{id: "child1",},[
-        React.createElement("h1",{},"heading1"),
-        React.createElement("h1",{},"heading223"),
-    ]),
-]);
-    //      this both are same
 
-{/* <div id="root">Not rendered</div>
-    <div id="parent">
-        <div class="child">
-            <h1></h1>
-            <h2></h2>
-        </div>
-        <div class="child1">
-            <h1></h1>
-            <h2></h2>
-        </div>
-    </div> */}
-  
-  // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(container);
+  root.render();
